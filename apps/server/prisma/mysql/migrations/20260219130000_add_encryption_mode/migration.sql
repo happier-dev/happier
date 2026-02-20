11ac44c8973ec22728cf0d8395b4db2d8c33d394
Leeroy Brun <leeroy.brun@gmail.com>
2026-02-20 23:27:25 +0100

    fix(server): make mysql encryptionModeUpdatedAt vitess-safe

--- apps/server/prisma/mysql/migrations/20260219130000_add_encryption_mode/migration.sql
+++ apps/server/prisma/mysql/migrations/20260219130000_add_encryption_mode/migration.sql
@@ -1,7 +1,8 @@
 -- AlterTable
 ALTER TABLE `Account` ADD COLUMN `encryptionMode` VARCHAR(191) NOT NULL DEFAULT 'e2ee';
-ALTER TABLE `Account` ADD COLUMN `encryptionModeUpdatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
+-- Vitess/Planetscale compatibility: avoid non-constant defaults in ALTER TABLE.
+ALTER TABLE `Account` ADD COLUMN `encryptionModeUpdatedAt` DATETIME(3) NOT NULL DEFAULT '1970-01-01 00:00:00.000';
+UPDATE `Account` SET `encryptionModeUpdatedAt` = `updatedAt` WHERE `encryptionModeUpdatedAt` = '1970-01-01 00:00:00.000';
 
 -- AlterTable
 ALTER TABLE `Session` ADD COLUMN `encryptionMode` VARCHAR(191) NOT NULL DEFAULT 'e2ee';
-
